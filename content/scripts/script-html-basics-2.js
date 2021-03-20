c7027ddd9e4985daf4a9afa1583d25050b75352c
FurryTr4sh <72644164+FurryTr4sh@users.noreply.github.com>
2021-03-20 11:57:22 +0800

    Add files via upload

--- content/scripts/script-html-basics-2.js
+++ content/scripts/script-html-basics-2.js
@@ -41,7 +41,7 @@ var correctAnswers
 function answer() {  // Функция подсчёта количества правильных ответов
 	correctAnswers = 0;
 	for (i of arr) {
-		document.getElementById('l' + i).setAttribute('class', 'a');  // Присваивание класса 'a' каждому элементу из массива для подсветки
+		document.getElementById('f' + i).setAttribute('class', 'a');  // Присваивание класса 'a' каждому элементу из массива для подсветки
 		if (document.getElementById('r' + i).checked == false) {		
 		}
 		else {
