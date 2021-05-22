cdc4a5ed3f582f869bf897b1aac6acc14b8cb332
FurryTr4sh <72644164+FurryTr4sh@users.noreply.github.com>
2021-05-22 23:28:02 +0800

    Implemented dark/light mode toggler

--- content/scripts/test-script.js
+++ content/scripts/test-script.js
@@ -35,3 +35,20 @@ $(function() { // Кнопка сброса выбранных radio будет 
 	var checked_radios = $('input[type="radio"]');
 	var amount_of_checked = checked_radios.length;
 });
+
+if (localStorage.getItem("ui") !== null) {
+	// Если в localStorage есть элемент, то включена светлая тема
+	$("body").addClass("body-light");
+	$(".nav").addClass("nav-light");
+	$(".white-text").addClass("white-text-light");
+	$("h3").addClass("h3-light");
+	$("label").addClass("label-light");
+}
+else {
+	// Иначе же будет включено тёмное оформление
+	$("body").removeClass("body-light");
+	$(".nav").removeClass("nav-light");
+	$(".white-text").removeClass("white-text-light");
+	$("h3").removeClass("h3-light");
+	$("label").removeClass("label-light");
+}
